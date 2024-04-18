ab9519b2915c9820302c69f714a2fe7d3a1cae5f
jay <jaymakadia2003@gmail.com>
2024-04-18 20:18:44 +0530

    signup and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import Income from './Components/Income.jsx';
 import Expense from './Components/Expense.jsx'
 import Loginpage from './Components/Loginpage.jsx';
+import Signup from './Components/Signup.jsx';
 
 function App() {
   return (
     <div style={{fontFamily:"LuxoraGrotesk-Regular"}}>
-      <Homepage/>
+      <Signup/>
     </div>
     
   );
