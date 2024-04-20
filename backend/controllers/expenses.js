c60e02b8ffa1ddd312791d90499e142ce8fd56b4
HRK1503 <kotechahitarth1503@gmail.com>
2024-04-20 14:22:13 +0530

    both models completed

--- backend/controllers/expenses.js
+++ backend/controllers/expenses.js
@@ -1,9 +1,9 @@
-const IncomeSchema = require("../models/incomeModel");
+const ExpenseSchema = require("../models/expenseModel");
 
-exports.addIncome=async(req,res) =>{
+exports.addExpense=async(req,res) =>{
     const {title,amount,category,description,date}=req.body;
 
-    const income=IncomeSchema({
+    const expense=ExpenseSchema({
         title,
         amount,
         category,
@@ -19,8 +19,8 @@ exports.addIncome=async(req,res) =>{
         if(amount<=0){
             return res.status(400).json({message:"Amount must be positive"})
         }
-        await income.save()
-        res.status(200).json({message:"Income added successfully"})
+        await expense.save()
+        res.status(200).json({message:"Expense added successfully"})
     }
     catch(e){
         console.error(e);
@@ -30,9 +30,9 @@ exports.addIncome=async(req,res) =>{
 }
 
 
-exports.getIncomes=async(req,res)=>{
+exports.getExpense=async(req,res)=>{
     try{
-        const incomes=await IncomeSchema.find().sort({createdAt:-1})
+        const incomes=await ExpenseSchema.find().sort({createdAt:-1})
         res.status(200).json(incomes)
     }
     catch(e){
@@ -41,10 +41,10 @@ exports.getIncomes=async(req,res)=>{
     }
 }
 
-exports.deleteIncomes=async(req,res)=>{
+exports.deleteExpense=async(req,res)=>{
         const {id}=req.params;
-        IncomeSchema.findByIdAndDelete(id).then((income)=>{
-            res.status(200).json({message:"Income deleted"})
+        ExpenseSchema.findByIdAndDelete(id).then((income)=>{
+            res.status(200).json({message:"Expense deleted"})
         }).catch((error)=>{
             console.error(e);
         res.status(500).json({ message: "Internal Server Error" });
